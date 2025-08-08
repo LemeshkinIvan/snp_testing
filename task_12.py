179aa70796c340260f36eb0051a8b9c3ab130fbb
Ivan Lemeshkin <106601642+LemeshkinIvan@users.noreply.github.com>
2025-08-08 12:30:21 +0300

    Update task_12.py

--- task_12.py
+++ task_12.py
@@ -1,8 +1,8 @@
 from task_11 import Dessert
 
 class JellyBean(Dessert):
-    def __init__(self, name = "Undefined", calories: any = None, flavor = 'Undefined flavor'):
-        super().__init__(name, calories)
+    def init(self, name="Undefined", calories=None, flavor='Undefined flavor'):
+        super().init(name, calories)
         self._flavor = flavor
 
     @property
@@ -18,48 +18,7 @@ class JellyBean(Dessert):
             print(str(e))
     
     def is_delicious(self):       
-        return not(self._flavor == "black licorise")
+        return self._flavor.lower() != "black licorice"
     
-    def __str__(self):
+    def str(self):
         return f"Desert has name: {self._name}\tcalories: {self._calories}\tflavor: {self._flavor}"
-    
-
-dessert = JellyBean()
-if not issubclass(dessert.__class__, JellyBean): raise Exception("Invalid inheritance")
-dessert.name = "test_name"
-print(dessert.name)
-
-dessert.name = "test_name2"
-print(dessert.name)
-
-if dessert.name != "test_name2": raise Exception("Setter for name is not working")
-dessert.calories = "test_calories"
-print(dessert.calories)
-
-dessert.calories = "test_calories2"
-print(dessert.calories)
-
-if dessert.calories != "test_calories2": raise Exception("Setter for calories is not working")
-print(dessert.is_delicious())
-
-if not dessert.is_delicious(): raise Exception("Invalid method result")
-dessert.flavor = "test_flavor"
-print(dessert.flavor)
-
-print(dessert.is_healthy())
-
-dessert.calories = 300
-print(dessert.calories)
-300
-print(dessert.is_healthy())
-
-if dessert.is_healthy(): raise Exception("Logical error. Method must return False")
-print(dessert.is_delicious())
-
-if not dessert.is_delicious(): raise Exception("Invalid method result")
-dessert.calories = 200
-print(dessert.calories)
-200
-print(dessert.is_healthy())
-
-if dessert.is_healthy(): raise Exception("Logical error. Method must return False")
